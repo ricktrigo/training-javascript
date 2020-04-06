aea5b5fc897f8e684da386585299d731c4b571d1
Ricardo Trigo <ricardo.and@hotmail.es>
2020-04-06 17:59:10 +0200

    Exos lab 01/04

---
+++ lab/01/04/script.js
@@ -0,0 +1,19 @@
+const var2 = "Ricardo";
+function displayBooleanTrue() {
+    const var1 = true;
+    console.log(var1);
+}
+
+function displayName() {
+    console.log(var2);
+}
+
+function displayNameUppercase() {
+    console.log(var2.toUpperCase());
+}
+
+function displaySum() {
+    const var3 = 10;
+    const var4 = 10.25;
+    console.log(var3+var4);
+}
